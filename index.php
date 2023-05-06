5386ffc0d3ffc6b62e12150da0d87711760d547c
adil_shah <adilkhan12324@gmail.com>
2023-05-07 01:03:55 +0500

    bank account holder app in php

--- index.php
+++ index.php
@@ -1,8 +1,40 @@
 <?php
-include 'database.php';
+class bank{
+  public $accountName;
+  public $accountHolder;
+  public $balance;
+  
+  public function __construct($an, $ah, $b)
+  {
+    $this->accountName = $an;
+    $this->accountHolder = $ah;
+    $this->balance = $b;
+    // echo 'asd';
+  }
+  public function deposite($amount){
+    echo "You have deposited PKR:". $this->balance += $amount;
+  }
+  public function withdraw($amount){
+    if ($this->balance <= $amount) {
+      echo "<br> Your account has Low balance which is : ". $this->balance;
+    }
+    else{
+       $this->balance -= $amount;
+       echo "<br>You have withdraw amount PKR: ".$amount;
+    }
+  }
+    public function getBalance(){
+      echo "<br>Your Remaining Balance is PKR: ".$this->balance;
+    }
 
-$obj = new Database();
-$obj->insert('user',["name" => "hoorams shah","email"=>"hooram@gmail.com"]);
-echo "Inserted Result is : ";
-print_r($obj->getResult());
+    public function accountInfo($address){
+      echo "<br>Account Holder Name is : ".$this->accountHolder.
+      "<br>Account Holder Address# ",$address; 
+    }
+}
+$obj = new bank(400,'ADIL SHAH',50);
+echo $obj->deposite(300);
+echo $obj->withdraw(300);
+echo $obj->getBalance();
+echo $obj->accountInfo(strtoupper('mardan'));
 ?>
